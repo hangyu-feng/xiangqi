45ab9ebc101f7c52a937826a36e9feecbe6a5d0a
Hangyu feng <hangyu.feng@mail.utoronto.ca>
2021-07-04 02:55:51 -0400

    initialize

---
+++ src/main/java/com/example/XiangQi/XiangQiApplication.java
@@ -0,0 +1,13 @@
+package com.example.XiangQi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiangQiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XiangQiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/XiangQi/XiangQiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.XiangQi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiangQiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
