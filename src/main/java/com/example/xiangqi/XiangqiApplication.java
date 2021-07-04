184dcef3f50a94fb07fbbe9ed6d34872ddd815d9
Hangyu feng <hangyu.feng@mail.utoronto.ca>
2021-07-04 03:11:59 -0400

    .

--- src/main/java/com/example/xiangqi/XiangqiApplication.java
+++ src/main/java/com/example/xiangqi/XiangqiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class XiangqiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(XiangqiApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(XiangqiApplication.class, args);
+  }
 
 }
